2774b5505d3a551a18fe5ef142d816443e073e51
BirtasevicLazar <lazar.birtasevic1@gmail.com>
2025-01-10 16:55:36 +0100

    tailwindcss

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,12 @@
-import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-
-    </>
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="bg-blue-500 text-white p-4 rounded shadow-lg">
+        Hello, Tailwind CSS!
+      </div>
+    </div>
   )
 }
 
